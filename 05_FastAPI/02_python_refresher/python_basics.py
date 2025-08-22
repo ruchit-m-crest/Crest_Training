427997e3152132de2b979c126ba711a6dc0fa3a9
ruchit-m-crest <ruchit.m@crestinfosystems.com>
2025-08-22 17:52:57 +0530

    updated the code

---
+++ 05_FastAPI/02_python_refresher/python_basics.py
@@ -0,0 +1,52 @@
+# cost = 10
+# print(cost)
+# tax_pr = 0.20
+
+# tax = cost * tax_pr
+# price = cost + tax
+# print(price)
+
+# # String
+# first_name = "ruchit"
+# print(f"Hi {first_name}!")
+
+# last_name = "Meshiya"
+# sentence = "Hi {} {}"
+# print(sentence.format(first_name, last_name))
+
+# # user input
+# name = input("Enter your name :")
+# print(name)
+
+# day = int(input("Enter days :"))
+# print(day / 7)
+
+#List 
+# mylist = [1,2,3,4,5,6]
+# print(mylist)
+
+# mylist.append(1000)
+# print(mylist)
+
+#sets
+# myset = {1,2,3,4,1,4,5}
+# print(myset)
+# print(len(myset))
+
+# for x in myset:
+#     print(x)
+
+#Tuple
+# mytuple= (1,2,3,4,5,6,7,8)
+# print(mytuple)
+
+# print(mytuple[0])
+
+#import 
+# import random
+# print(random.randint(1,10))
+
+# import math
+# square_root = math.sqrt(64)
+# print(square_root)
+
